9a81fc1aa18c4704cb892b05a782d1fa1be08760
yurika abe <yurika.abe0@gmail.com>
2023-04-10 21:39:22 +0900

    4-10 カリキュラム

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(NewsController::class)->prefix('admin')->name('news.')->group(function(){
+    Route::get('news/create', 'add')->name('add');
+});
+
+Route::controller(SelfProfileController::class)->prefix('admin')->name('create.')->group(function(){
+    Route::get('profile/create', 'add')->name('create');
+    Route::get('profile/edit', 'edit')->name('edit');
+});
